cc68faed1e4a31537daee834785f5164ccd4ddbe
Mac16 <gilouthomas@gmail.com>
2022-07-06 17:28:15 +0200

    hide-waiting-gif added

--- src/process-main/system/messages.js
+++ src/process-main/system/messages.js
@@ -30,3 +30,8 @@ ipcMain.on('open-confirmation-dialog', (event, arg) => {
 ipcMain.on('error-dialog', (event, arg) => {  
   dialog.showErrorBox(arg[0], arg[1])  
 })
+
+ipcMain.on('hide-waiting-gif', (event, arg) => {  
+  console.log('remove waiting sent')
+  event.sender.send('remove-waiting-gif',null) 
+})
