b12011ac9ceb23ea12c621b0515ea4be77355aba
Mac16 <gilouthomas@gmail.com>
2022-08-13 18:31:57 +0200

    save-igc event added

--- src/process-main/files-utils/open-file.js
+++ src/process-main/files-utils/open-file.js
@@ -1,5 +1,6 @@
 const {ipcMain} = require('electron')
 const { dialog } = require('electron')
+const fs = require('fs')
 
 ipcMain.on('open-file', (event, arg) => {
   let fileChoosed = null
@@ -65,4 +66,31 @@ ipcMain.on('open-directory', (event,arg) => {
       folderChoosed = path[0]
   }
   event.returnValue = folderChoosed 
+})
+
+ipcMain.on('save-igc', (event, stringIgc) => {
+  let resultMsg = null
+  filename = dialog.showSaveDialog(
+    { title: 'Export IGC',
+      filters: [{
+        name: 'IGC format',
+        extensions: ['igc']
+      }]
+    }
+  )
+  .then(result => {
+      filename = result.filePath
+      if (filename === undefined) {
+        event.returnValue = 'Error : the user clicked the btn but didn\'t created a file'        
+      }
+      fs.writeFile(filename, stringIgc, (err) => {
+        if (err) {
+          event.returnValue = 'Error : An error ocurred with file creation ' + err.message          
+        }
+        event.returnValue = result.filePath
+      })      
+    })
+  .catch(err => {
+    event.returnValue = 'Error : '+err
+    })
 })
