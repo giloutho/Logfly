c38a9229cdbf8c1de3c4168b2cf17ac2b616ea07
Mac16 <gilouthomas@gmail.com>
2022-08-15 23:20:23 +0200

    FlyXC is a modal window

--- src/views/js/flyxc.js
+++ src/views/js/flyxc.js
@@ -4,38 +4,26 @@ var i18n = require('../../lang/gettext.js')()
 var Mustache = require('mustache')
 var Store = require('electron-store')
 var store = new Store()
-let menuFill = require('../../views/tpl/sidebar.js')
-let btnMenu = document.getElementById('toggleMenu')
 
-iniForm()
+const currLang = store.get('lang')
+i18n.setMessages('messages', currLang, store.get('langmsg'))
+i18n.setLocale(currLang)    
+const btnClose = document.getElementById('bt-close')
+btnClose.innerHTML = i18n.gettext('Close')
+btnClose.addEventListener('click',(event) => {
+    window.close()
+})  
+/**
+ * ATTENTION aux caractères d'échappement venus de Logfly5
+ * "urlvisu": "https\\://flyxc.app/?track\\=",
+ * "urllogflyigc": "http\\://www.logfly.org/Visu/",
+ * NE PAS RECUPERER, écrire en dur
+ */
+let srcPath = store.get('urlvisu')+store.get('urllogflyigc')+store.get('igcVisu')
+console.log(srcPath)
+// let srcPath = store.get('urlvisu')
+console.log('mod : https://flyxc.app/?track=http://logfly.org/Visu/20220712112212_885.igc')
+console.log('cal : '+srcPath)
+//srcPath = 'https://flyxc.app/?track=http://www.logfly.org/Visu/logfly6.igc'
+document.getElementById('webframe').src = srcPath
 
-function iniForm() {
-    const currLang = store.get('lang')
-    i18n.setMessages('messages', currLang, store.get('langmsg'))
-    i18n.setLocale(currLang)    
-    let menuOptions = menuFill.fillMenuOptions(i18n)
-    $.get('../../views/tpl/sidebar.html', function(templates) { 
-        var template = $(templates).filter('#temp-menu').html();  
-        var rendered = Mustache.render(template, menuOptions)
-      //  console.log(template)
-        document.getElementById('target-sidebar').innerHTML = rendered
-    })
-    let srcPath = store.get('urlvisu')+store.get('urllogflyigc')+store.get('igcVisu')
-    console.log('mod : https://flyxc.app/?track=http://logfly.org/Visu/20220712112212_885.igc')
-    console.log('cal : '+srcPath)
-    document.getElementById('webframe').src = srcPath
-}
-
-// Calls up the relevant page 
-function callPage(pageName) {
-    ipcRenderer.send("changeWindow", pageName);    // main.js
-}
-
-btnMenu.addEventListener('click', (event) => {
-    if (btnMenu.innerHTML === "Menu On") {
-        btnMenu.innerHTML = "Menu Off";
-    } else {
-        btnMenu.innerHTML = "Menu On";
-    }
-    $('#sidebar').toggleClass('active');
-})
