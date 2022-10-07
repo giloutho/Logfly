9bc33ac51cf063a3e5c4b53103dda089bcc34560
Mac16 <gilouthomas@gmail.com>
2022-10-07 23:03:46 +0200

    Name changed for 'gpsdumpMac32_54'

--- src/settings/gpsdump-name.js
+++ src/settings/gpsdump-name.js
@@ -1,6 +1,6 @@
 function getNames() {
     let gpsdumpList = {
-        'mac32' : 'GpsDump32_54',
+        'mac32' : 'gpsdumpMac32_54',
         'mac64' : 'gpsdumpMac64_14',
         'win' : 'GpsDump542.exe',
         'linux' : 'gpsdumpLin64_3'    
