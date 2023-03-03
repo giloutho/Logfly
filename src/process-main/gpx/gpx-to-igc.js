537d3e05783a3e414c38a6cf9ef223b0f29623e5
Mac16 <gilouthomas@gmail.com>
2023-03-03 21:50:52 +0100

    Bugs fixed

--- src/process-main/gpx/gpx-to-igc.js
+++ src/process-main/gpx/gpx-to-igc.js
@@ -18,11 +18,12 @@ function encodeIGC(gpxString, logbook) {
     parser.parse(gpxString)
     let nbPt = 0
     let parsedTracks = parser.tracks
-    if (parsedTracks.length > 0) {
+    const nbTracks = parsedTracks.length
+    if (nbTracks > 0) {
+        // File header definition
         const firstTrack = parsedTracks[0]
         const startTime = firstTrack.points[0].time
-        const startDate = String(startTime.getDate()).padStart(2, '0')+String((startTime.getMonth()+1)).padStart(2, '0')+startTime.getFullYear()
-
+        const startDate = String(startTime.getDate()).padStart(2, '0')+String((startTime.getMonth()+1)).padStart(2, '0')+startTime.getFullYear().toString().slice(-2)
         stringIGC += 'AXLF'+CrLf
         stringIGC += 'HFDTE'+startDate+CrLf          
         // for external tracks we have not pilot and glider name
@@ -33,30 +34,29 @@ function encodeIGC(gpxString, logbook) {
         stringIGC += 'HOCIDCOMPETITIONID:'+CrLf         
         stringIGC += 'HOCCLCOMPETITION CLASS:'+CrLf 
         stringIGC += 'HOSITSite:'+CrLf 
-        firstTrack.points.forEach(function(pt){
-            nbPt++
-            // let strDate = String(pt.time.getDate()).padStart(2, '0')+'/'+String((pt.time.getMonth()+1)).padStart(2, '0')+'/'+pt.time.getFullYear()
-            //let strTime = String(pt.time.getHours()).padStart(2, '0')+String(pt.time.getMinutes()).padStart(2, '0')+String(pt.time.getSeconds()).padStart(2, '0')        
-            let igcLat = Lat_Dd_IGC(pt.lat)
-            let igcLong = Long_Dd_IGC(pt.lon)
-            let utcTime = fixUTC(pt.time)
-            let bRecord = 'B'+utcTime+igcLat+igcLong+'A00000'+pt.ele.toString().padStart(5, '0')
-            stringIGC += bRecord+CrLf
-        })
-
-        if(parsedTracks.length > 1) {
-            // iteration sur les tracks supplémentaires
-        } else {
-            console.log('Une seule trace')
+        for (let index = 0; index < nbTracks; index++) {
+            const currTrack = parsedTracks[index]
+            currTrack.points.forEach(function(pt){
+                nbPt++
+                // let strDate = String(pt.time.getDate()).padStart(2, '0')+'/'+String((pt.time.getMonth()+1)).padStart(2, '0')+'/'+pt.time.getFullYear()
+                //let strTime = String(pt.time.getHours()).padStart(2, '0')+String(pt.time.getMinutes()).padStart(2, '0')+String(pt.time.getSeconds()).padStart(2, '0')        
+                let igcLat = Lat_Dd_IGC(pt.lat)
+                let igcLong = Long_Dd_IGC(pt.lon)
+                let utcTime = fixUTC(pt.time)
+                // Elevation can be a real number like this :
+                // <trkpt lat="45.890532" lon="6.450395"><ele>1812.959</ele><time>2013-09-25T11:48:32Z</time>
+                let intAlt = Math.ceil(pt.ele)
+                let strAlt = intAlt.toString().padStart(5, '0')
+                let bRecord = 'B'+utcTime+igcLat+igcLong+'A00000'+strAlt
+                stringIGC += bRecord+CrLf
+            })            
         }
-
-        // footer
+        // File footer definition
         stringIGC += 'LXLF Logfly 6'+CrLf 
         const genDate = new Date();  
-        // utiliser moment
         let strDate = String(genDate.getDate()).padStart(2, '0')+'-'+String((genDate.getMonth()+1)).padStart(2, '0')+'-'+genDate.getFullYear()
         let strTime = String(genDate.getHours()).padStart(2, '0')+':'+String(genDate.getMinutes()).padStart(2, '0')+':'+String(genDate.getSeconds()).padStart(2, '0')     
-        stringIGC += 'LXLF generated '+strDate+' '+strTime+CrLf      
+        stringIGC += 'LXLF generated '+strDate+' '+strTime+CrLf   
     }
     result.igcString = stringIGC
     result.nbPoints = nbPt
@@ -82,12 +82,12 @@ function Lat_Dd_IGC(dLat)  {
         // format with 3 decimals
         const dMin = (Math.round(fMin * 1000) / 1000).toFixed(3)
         const sMin = dMin.toString()
-        igcLat = fDeg.toString().padStart(2, '0')+sMin.split(".")[0]+sMin.split(".")[1]
+        // console.log(fDeg.toString().padStart(2, '0')+' '+sMin.split(".")[0].padStart(2, '0')+' '+sMin.split(".")[1])
+        igcLat = fDeg.toString().padStart(2, '0')+sMin.split(".")[0].padStart(2, '0')+sMin.split(".")[1]
         if (dLat < 0)
             igcLat += 'S'
         else
         igcLat += 'N'
-       // console.log(dLat+' '+AbsLat+' '+fDeg+' '+fMin+' '+dMin+' '+sMin)
     } catch (error) {
         igcLat = ''
     }        
@@ -105,7 +105,7 @@ function Long_Dd_IGC(dLong)  {
         // format with 3 decimals
         const dMin = (Math.round(fMin * 1000) / 1000).toFixed(3)
         const sMin = dMin.toString()
-        igcLong = fDeg.toString().padStart(3, '0')+sMin.split(".")[0]+sMin.split(".")[1]
+        igcLong = fDeg.toString().padStart(3, '0')+sMin.split(".")[0].padStart(2, '0')+sMin.split(".")[1]
         if (dLong < 0) 
             igcLong += 'W'
         else
