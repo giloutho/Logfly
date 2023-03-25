eee7592622bbd3a84a959f22601ecbc8266f1d1d
Mac16 <gilouthomas@gmail.com>
2023-03-25 20:00:08 +0100

    Removed

--- src/process-main/openair/read-oa.js
+++
@@ -1,9 +0,0 @@
-const {ipcMain} = require('electron')
-const OpenDecoder  = require('../../utils/open-decoder.js')
-
-ipcMain.on('read-oa', (event, arg) => {
-  const currOpenAir = new OpenDecoder(arg[0],arg[1])
-  // True to active console report
-  currOpenAir.decode()     
-  event.returnValue = currOpenAir
-})
